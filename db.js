d33657f718f263f58bda602001b118b39d5f5efa
adnanrahic <ado.raha198@gmail.com>
2017-04-29 02:34:39 +0200

    testing

--- db.js
+++ db.js
@@ -1,5 +1,4 @@
 var mongoose = require('mongoose');
 var username = process.env.USERNAME || 'sita';
 var password = process.env.PASSWORD || 'kmetboy';
-// mongoose.connect('mongodb://'+ username +':'+ password +'@ds135830.mlab.com:35830/myblog');
-mongoose.connect('mongodb://admin:supersecret@ds135830.mlab.com:35830/myblog');
+mongoose.connect('mongodb://'+ username +':'+ password +'@ds135830.mlab.com:35830/myblog');
