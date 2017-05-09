5b048d33b068e2461f768c00b745e5d0fc6817bc
adnanrahic <ado.raha198@gmail.com>
2017-05-09 12:35:14 +0200

    some more testing

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
-var SECRET;
-if (process.env.PROD !== 1) {
-  SECRET = require('./.env/dev').SECRET;
-}
+// var SECRET = require('./.env/dev').SECRET;
 module.exports = {
   'secret': process.env.SECRET || SECRET
 };
