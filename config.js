602a9a54a87a2044662b07b41fb52d2fc8ff0005
adnanrahic <ado.raha198@gmail.com>
2017-05-09 12:39:06 +0200

    and another time with the env testing

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
-// var SECRET = require('./.env/dev').SECRET;
+var SECRET;
+if (process.env.PROD !== 1) {
+  SECRET = require('./.env/dev').SECRET;
+}
 module.exports = {
   'secret': process.env.SECRET || SECRET
 };
--- db.js
+++ db.js
@@ -1,5 +1,8 @@
 var mongoose = require('mongoose');
-var envDev = require('./.env/dev');
+var envDev;
+if (process.env.PROD !== 1) {
+  envDev = require('./.env/dev');
+}
 var username = process.env.USERNAME || envDev.USERNAME;
 var password = process.env.PASSWORD || envDev.PASSWORD;
 mongoose.connect('mongodb://'+ username +':'+ password +'@ds135830.mlab.com:35830/myblog');
